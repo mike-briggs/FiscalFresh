307fe3beef12e8be06b52a9116b56da2ae0717b1
James McDonald <james_mcd98@hotmail.com>
2020-02-08 21:35:01 -0500

    Updated database file with new collections and made new methods

--- database.py
+++ database.py
@@ -10,8 +10,12 @@ with open('credentials.json', 'r') as f:
 dbconnection = "mongodb+srv://qhacks2020:{}@cluster0-kq8wa.gcp.mongodb.net/test?retryWrites=true&w=majority".format(dbKey)
 client = MongoClient(dbconnection)
 
-db = client.get_database('recipe_history')
-records = db.history
+recipe_history = client.get_database('recipe_history')
+recipe_history_records = recipe_history.history
+
+pantry = client.get_database('pantry')
+pantry_records = pantry.history
+
 print('Connected to database successfully')
 # class database(object):
         
@@ -20,12 +24,12 @@ print('Connected to database successfully')
 # OUT: list of recipies associated with that user
 def get_history(email):
 
-    if (records.count_documents({'email':email}) != 1):
+    if (recipe_history_records.count_documents({'email':email}) != 1):
         return {
             'err':"email not found"
         }
 
-    entry = records.find_one({'email': email})
+    entry = recipe_history_records.find_one({'email': email})
     return (json.dumps(entry['recipes']))
 
 #IN: email, array of recipes
@@ -37,23 +41,60 @@ def add_history(request_body):
     email = request_body['email']
     additions = request_body['recipes']
     # add document
-    if records.count_documents({'email':email}) == 0:
+    if recipe_history_records.count_documents({'email':email}) == 0:
         new_entry={
             'email':email,
             'recipes': [additions]
         }
-        records.insert_one(new_entry)
+        recipe_history_records.insert_one(new_entry)
 
     # edit document that already exists
     else:
-        entry = records.find_one({'email':email})
+        entry = recipe_history_records.find_one({'email':email})
         for recipe in entry['recipes']:
             if recipe['id'] == additions['id']:
                 return {
                     'result': "success"
                 }
         entry['recipes'].append(additions)        
-        records.update_one({'email':email}, {"$set":{'recipes': entry['recipes']}})
+        recipe_history_records.update_one({'email':email}, {"$set":{'recipes': entry['recipes']}})
     return {
         'result': "success"
     }
+
+def get_pantry(email):
+    if (pantry_records.count_documents({'email':email}) != 1):
+        return {
+            'err':"email not found"
+        }
+
+    entry = pantry_records.find_one({'email': email})
+    return (json.dumps(entry['items']))
+
+def add_pantry(request_body):
+    if not request_body:
+        request_body = request.form
+    
+    email = request_body['email']
+    additions = request_body['pantryItems']
+
+    if pantry_records.count_documents({'email':email}) == 0:
+        new_entry={
+            'email':email,
+            'items':[additions]
+        }
+        pantry_records.insert_one(new_entry)
+
+    else:
+        entry = pantry_records.find_one({'email':email})
+        for item in entry['items']:
+            if item['id'] == additions['id']:
+                return{
+                    'result':"success"
+                }
+        entry['items'].append(additions)
+        pantry_records.update_one({'email':email}, {"$set":{'items': entry['items']}})
+        return{
+            'result':"success"
+        }
+
